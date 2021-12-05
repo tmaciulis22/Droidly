e5e0cdc6c32859d7415b9a7015788a6b1e0b3ad1
Tautvydas Mačiulis <maciulis.tautvydas22@gmail.com>
2021-12-05 15:25:39 +0200

    Init builder project

--- droidly-app/app/src/main/java/com/example/droidly/MainActivity.kt
+++ droidly-app/app/src/main/java/com/example/droidly/MainActivity.kt
@@ -3,11 +3,8 @@ package com.example.droidly
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.droidly.ui.theme.DroidlyTheme
 
 class MainActivity : ComponentActivity() {
